a34df4d1cc282bdb3cb310c42d108f87d976b9ea
YaseiTanuki <tanuki1707@gmail.com>
2024-05-08 09:25:38 +0700

    add statistic class

--- main.py
+++ main.py
@@ -15,7 +15,17 @@ bot = commands.Bot(command_prefix="r! ", intents=intents)
 async def on_ready():
     await bot.load_extension('cogs.ManageMessages')
     await bot.load_extension('cogs.ManageMember')
+    await bot.load_extension('cogs.Statistic')
+    await bot.load_extension('cogs.Play')
     print("Bot is now ready")
+
+@bot.command()
+async def join(ctx):
+    await ctx.author.voice.channel.connect()
+
+@bot.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
 
 # Start bot (must be the last function to call)
 bot.run(os.getenv('DISCORD_APP_TOKEN'))
